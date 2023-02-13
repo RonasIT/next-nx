83230aaedcb90d8b41a09a5cea394b2e0653d356
Alexander Storozhevsky <astorozhevsky@ronasit.com>
2023-02-13 15:55:13 +0100

    feat(next): add tests

--- packages/next-nx/src/generators/preset/generator.spec.ts
+++ packages/next-nx/src/generators/preset/generator.spec.ts
@@ -14,11 +14,18 @@ describe('preset generator', () => {
 
   it('should be defined in project configuration successfully', async () => {
     await generator(appTree, options);
+
     const config = readProjectConfiguration(appTree, 'test');
     expect(config).toBeDefined();
   });
 
   it('should run successfully', async () => {
+    const result = await generator(appTree, options);
+
+    expect(result).toBeInstanceOf(Function);
+  });
+
+  it('should replace prettierrc config', async () => {
     await generator(appTree, options);
 
     expect(appTree.exists('.prettierrc')).toBeFalsy();
