42a82276968599b41089c5ded4b9249b13ed471c
Alexander Storozhevsky <astorozhevsky@ronasit.com>
2023-02-16 15:24:02 +0100

    fix: async function should have await expression
    refs: https://deepsource.io/gh/RonasIT/nx/issue/JS-0116/occurrences/

--- packages/expo-nx/src/generators/application/generator.ts
+++ packages/expo-nx/src/generators/application/generator.ts
@@ -2,6 +2,6 @@ import { Tree } from '@nrwl/devkit';
 import { ApplicationGeneratorSchema } from './schema';
 import { expoApplicationGenerator } from '@nrwl/expo';
 
-export default async function (tree: Tree, options: ApplicationGeneratorSchema) {
+export default function (tree: Tree, options: ApplicationGeneratorSchema) {
   return expoApplicationGenerator(tree, options);
 }
--- packages/next-nx/src/generators/application/generator.ts
+++ packages/next-nx/src/generators/application/generator.ts
@@ -2,6 +2,6 @@ import { getWorkspaceLayout, Tree } from '@nrwl/devkit';
 import { ApplicationGeneratorSchema } from './schema';
 import { applicationGenerator } from '@nrwl/next';
 
-export default async function (tree: Tree, options: ApplicationGeneratorSchema) {
+export default function (tree: Tree, options: ApplicationGeneratorSchema) {
   return applicationGenerator(tree, options);
 }
