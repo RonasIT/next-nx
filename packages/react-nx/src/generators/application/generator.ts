b57b5fb7c1f8734db9838cc6ba093107c14ebe25
Alexander Storozhevsky <astorozhevsky@ronasit.com>
2023-05-31 15:38:47 +0600

    fix: prefer the usage of regular expression literals over the RegExp constructor JS-0115
    refs: https://app.deepsource.com/gh/RonasIT/nx/issue/JS-0115/occurrences

--- packages/react-nx/src/generators/application/generator.ts
+++ packages/react-nx/src/generators/application/generator.ts
@@ -20,7 +20,7 @@ interface NormalizedSchema extends ApplicationGeneratorSchema {
 function normalizeOptions(tree: Tree, options: ApplicationGeneratorSchema): NormalizedSchema {
   const name = names(options.name).fileName;
   const projectDirectory = options.directory ? `${names(options.directory).fileName}/${name}` : name;
-  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
+  const projectName = projectDirectory.replace(/\//g, '-');
   const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
   const parsedTags = options.tags ? options.tags.split(',').map((s) => s.trim()) : [];
 
